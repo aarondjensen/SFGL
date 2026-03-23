9853d0c3b82d7e5e6e7063db491de3ae213c28ec
aarondjensen <85518801+aarondjensen@users.noreply.github.com>
2026-03-23 13:14:14 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Trophy,  Award, Users, DollarSign, Calendar, Settings, BarChart2 } from 'lucide-react';
 
-import { DialogProvider } from './components/DialogContext';
-import { ErrorBoundary }  from './components/ErrorBoundary';
-import { StandingsView }  from './components/StandingsView';
-import { ResultsView }    from './components/ResultsView';
-import { RostersView }    from './components/RostersView';
-import { TransactionsView } from './components/TransactionsView';
-import { TournamentsView }  from './components/TournamentsView';
-import { AdminView }        from './components/AdminView';
-import { StatsView }         from './components/StatsView';
-import LoginPage            from './components/LoginPage';
+import { DialogProvider } from './pages/DialogContext';
+import { ErrorBoundary }  from './pages/ErrorBoundary';
+import { StandingsView }  from './pages/StandingsView';
+import { ResultsView }    from './pages/ResultsView';
+import { RostersView }    from './pages/RostersView';
+import { TransactionsView } from './pages/TransactionsView';
+import { TournamentsView }  from './pages/TournamentsView';
+import { AdminView }        from './pages/AdminView';
+import LoginPage            from './pages/LoginPage';
+import { StatsView }         from './pages/StatsView';
 
 import { useLeague }       from './hooks';
 import { hashPassword, getSegmentByDate, fetchFirstTeeTime } from './utils';
