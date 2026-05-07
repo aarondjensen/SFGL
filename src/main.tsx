a6234d1b73e60696c17aa66e98bb63c44d682353
aarondjensen <85518801+aarondjensen@users.noreply.github.com>
2026-05-06 20:52:38 -0400

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import './app-global.css'   // Fix 4: static CSS replacing runtime style injections
+import './app-global.css'
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(
